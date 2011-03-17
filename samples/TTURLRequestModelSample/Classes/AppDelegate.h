aa6eb72b38aef762d6ad1a655e94e86a8bb2edf9
Savanah Cummings <savanah.cummings@gmail.com>
2011-03-16 23:03:17 -0400

    Minimal three20 TTURLRequestModel demo app
    Demonstrating three20 features:
    - TTURLRequestModel for fetching an array of results in JSON format
    - Minimal TTTableViewController with custom cells
    - Using TTListDataSource setCellClass:forItemClass: to avoid custom item and datasource classes

--- samples/TTURLRequestModelSample/Classes/AppDelegate.h
+++ samples/TTURLRequestModelSample/Classes/AppDelegate.h
@@ -1,18 +1,17 @@
 //
-//  TTURLRequestModelSampleAppDelegate.h
+//  AppDelegate.h
 //  TTURLRequestModelSample
 //
 //  Created by Savanah Cummings on 3/15/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Three20/Three20.h>
 
-@interface TTURLRequestModelSampleAppDelegate : NSObject <UIApplicationDelegate> {
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
     UIWindow *window;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
-
 @end
 
--- samples/TTURLRequestModelSample/main.m
+++ samples/TTURLRequestModelSample/main.m
@@ -6,12 +6,13 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Three20/Three20.h>
+#import "AppDelegate.h"
 
 int main(int argc, char *argv[]) {
     
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, nil);
-    [pool release];
-    return retVal;
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+  [pool release];
+  return retVal;
 }
